2004b774f1a29fbf0eef51d70dbb6077de01d7b5
povilas taraikavicius <ptpecka@gmail.com>
2023-12-14 20:46:30 +0200

    add menu and table

---
+++ menu.py
@@ -0,0 +1,43 @@
+VALID_COFFEE = {
+    "Regular": 2.5,
+    "Americano": 4.5,
+    "Cappuccino": 5.5,
+    "Expresso": 5.5,
+    "Mocha": 6.5,
+    "macchiato": 7.5,
+}
+
+
+VALID_BREAKFAST = {
+    "Pancakes": {
+        "weight": "100g",
+        "prep.time": "15min",
+        "calories": 200,
+        "price": 7.25,
+    },
+    "Eggs Benedict": {
+        "weight": "50g",
+        "prep.time": "10min",
+        "calories": 50,
+        "price": 8.50,
+    },
+    "Parfait": {
+        "weight": "50g",
+        "prep.time": "10min",
+        "calories": 50,
+        "price": 2.50,
+    },
+    "Quiche": {
+        "weight": "50g",
+        "prep.time": "10min",
+        "calories": 50,
+        "price": 9.50,
+    },
+}
+
+VALID_DESSERTS = {
+    "Cheesecake": 3.5,
+    "Donuts": 3.7,
+    "Chess pie": 4.5,
+    "Blondies": 5.5,
+}
---
+++ table.py
@@ -0,0 +1,15 @@
+
+DOUBLE_TABLES = {
+    1: "free",
+    2: "free",
+    3: "free",
+    4: "free",
+}
+FAMILY_TABLES = {
+    1: "free",
+    2: "free",
+}
+
+PARTY_TABLES = {
+    1: "free",
+}
